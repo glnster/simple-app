361b733b3ce71969d372cede062ae69e53e0be1b
Glenn Cueto <gcueto@linkedin.com>
2021-07-28 18:07:31 -0700

    update no hooks test

--- tests/unit/unit-test.js
+++ tests/unit/unit-test.js
@@ -1,8 +1,6 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
 
-module('Unit | foo', function (hooks) {
-  setupTest(hooks);
+module('Unit | foo', function () {
   test('foo', function (assert) {
     assert.equal('bar', 'bar');
   });
